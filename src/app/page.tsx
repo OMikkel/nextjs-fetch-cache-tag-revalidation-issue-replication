8bc7b49d71bd41f2cef8366d76ad4a682f527f18
OMikkel <mikkelmaae@gmail.com>
2024-01-30 13:52:49 +0100

    Use time instead of user data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,37 @@
-import { getUser } from "@/lib/getUser";
-import { revalidateUser, updateUserName } from "./actions";
-import { promises } from "fs";
+import { getTime } from "@/lib/getTime";
 import { revalidateTag } from "next/cache";
 
 export default async function Home() {
-	const databaseContent = await promises.readFile(
-		process.cwd() + "/src/app/user.json",
-		"utf-8"
-	);
-	const user = await getUser("johndoe+123@gmail.com");
+	const currentTime = new Date().toISOString();
+	const time = await getTime();
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1 className="text-2xl text-slate-900 font-semibold">
 				Replication of Vercel fetch cache revalidation issue
 			</h1>
-			<div className="grid grid-cols-2">
-				<div>
-					<h2 className="text-xl text-slate-900 font-semibold">
-						User data fetched from database
-					</h2>
-					<pre className="text-slate-900">{databaseContent}</pre>
-					<p>Revalidation tag: {JSON.parse(databaseContent).email}</p>
-				</div>
-				<div>
-					<h2 className="text-xl text-slate-900 font-semibold">
-						User data fetched from API
-					</h2>
-					<pre className="text-slate-900">{JSON.stringify(user, null, 2)}</pre>
-					<p>Revalidation tag: {user.email}</p>
-				</div>
-			</div>
-			<form action={updateUserName} className="flex items-end gap-2">
-				<div className="space-y-1">
-					<label htmlFor="name">User Name:</label>
-					<input
-						type="text"
-						name="name"
-						id="name"
-						defaultValue={user.name}
-						className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring ring-ring ring-1 disabled:cursor-not-allowed disabled:opacity-50"
-					/>
-				</div>
-				<input type="hidden" name="email" id="email" value={user.email} />
-				<button
-					type="submit"
-					className="bg-blue-500 px-4 py-1.5 rounded-md text-white"
-				>
-					Update name
-				</button>
-			</form>
 			<div className="grid grid-cols-2 gap-10">
+				<div>
+					<h2 className="text-xl text-slate-900 font-semibold">Current time</h2>
+					<pre className="text-slate-900">{currentTime}</pre>
+					<p>Revalidation tag: {"tag-with+123"}</p>
+				</div>
+				<div>
+					<h2 className="text-xl text-slate-900 font-semibold">
+						Time from API
+					</h2>
+					<pre className="text-slate-900">{time.time}</pre>
+					<p>Revalidation tag: {"tag-with+123"}</p>
+				</div>
+
 				<form
 					action={async () => {
 						"use server";
-						revalidateTag(encodeURIComponent(`user-${user.email}`));
+						revalidateTag(encodeURIComponent("tag-with+123"));
 					}}
 				>
 					<p>
-						Tag: <code>{encodeURIComponent(user.email)}</code>
+						Tag: <code>{encodeURIComponent("tag-with+123")}</code>
 					</p>
 					<button
 						type="submit"
@@ -70,11 +43,11 @@ export default async function Home() {
 				<form
 					action={async () => {
 						"use server";
-						revalidateTag(`user-${user.email}`);
+						revalidateTag("tag-with+123");
 					}}
 				>
 					<p>
-						Tag: <code>{user.email}</code>
+						Tag: <code>{"tag-with+123"}</code>
 					</p>
 					<button
 						type="submit"
---
+++ src/lib/getTime.ts
@@ -0,0 +1,13 @@
+export const getTime = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/time`, {
+        next: {
+            tags: ["tag-with+123"]
+        }
+    });
+    if (!response.ok) return {
+        time: "No Date"
+    }
+    const data = await response.json();
+
+    return data;
+}
